31d3a4e28645389151bdc77389d29036ac9149e7
Frankmau5 <mulvie@protonmail.com>
2020-02-02 13:00:48 +0000

    Version 1.0.0.0
    This is the first release of TorrentViewer.

---
+++ Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TorrentEdtior
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // open button
+            OpenFileDialog ofd = new OpenFileDialog();
+            ofd.Title = "Please select a torrent file to open.";
+            ofd.Filter = "Torrent files (*.torrent) | *.torrent; ";
+         
+            if (ofd.ShowDialog() == DialogResult.OK)
+            {
+                string torrentFilePath = ofd.FileName;
+                // thread needed
+                myTorrent mt = new myTorrent(torrentFilePath);
+                var dataFromTorrent = mt.getTorrentFileInfo();
+
+                if (dataFromTorrent != null)
+                {
+                    this.richTextBox1.Text = dataFromTorrent; // displays info
+                }
+                    mt = null;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // about button
+            AboutBox1 box = new AboutBox1();
+            box.ShowDialog();
+        }
+    }
+}
---
+++ myTorrent.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using MonoTorrent;
+// add msg box 
+
+namespace TorrentEdtior
+{
+    class myTorrent
+    {
+        public String Path;
+
+        public myTorrent() { }
+        public myTorrent(String path) {
+            Path = path;
+
+        }
+
+
+        private string _getTorrentData() {
+            Torrent torrent = Torrent.Load(this.Path);
+            string torrentMetaData = "Files:\n";
+            foreach (var tFile in torrent.Files)
+            {
+                torrentMetaData = torrentMetaData + tFile.Path + "\n";
+            }
+
+            torrentMetaData = torrentMetaData + "\n" + "Other info\n";
+            torrentMetaData = torrentMetaData + "Created by: " + torrent.CreatedBy + "\n";
+            torrentMetaData = torrentMetaData + "Creation date: " + torrent.CreationDate + "\n";
+            torrentMetaData = torrentMetaData + "Publisher: " + torrent.Publisher + "\n";
+            torrentMetaData = torrentMetaData + "Publisher Url: " + torrent.PublisherUrl + "\n";
+            torrentMetaData = torrentMetaData + "SHA1: " + torrent.SHA1 + "\n";
+            torrentMetaData = torrentMetaData + "Size: " + torrent.Size.ToString() + "\n";
+            torrentMetaData = torrentMetaData + "Source: " + torrent.Source + "\n";
+            torrentMetaData = torrentMetaData + "Info Hash: " + torrent.InfoHash + "\n";
+            torrentMetaData = torrentMetaData + "Is Private: " + torrent.IsPrivate + "\n";
+            torrentMetaData = torrentMetaData + "Comment: " + torrent.Comment + "\n";
+            return torrentMetaData;
+        }
+        public String getTorrentFileInfo() {
+
+
+            if (!String.IsNullOrEmpty(this.Path)) {
+                FileInfo f = new FileInfo(this.Path);
+                if (f.Length < 1000000)
+                {
+                    return _getTorrentData();
+                }
+                else
+                {
+                    // display msg
+                    string message = "This file is bigger then an 1MB. This may take sometime.";
+                    string caption = "File may not be able to be open";
+                    MessageBoxButtons buttons = MessageBoxButtons.OK;
+
+                    MessageBox.Show(message, caption, buttons);
+                    return _getTorrentData();
+                    
+                }
+
+
+                
+            }
+            else
+            {
+                return null;
+            } 
+        }
+
+    }
+}
